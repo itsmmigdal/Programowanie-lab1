903d492830a1e23cd0a38ebc771fe293623907e6
itslisek <michal.migdal@microsoft.wsei.edu.pl>
2021-03-18 14:02:29 +0100

    SPOJ_1 DOD

---
+++ Suma/Suma/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SPOJ1_Suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int suma = a + b;
+            Console.WriteLine(suma));
+        }
+    }
+}
