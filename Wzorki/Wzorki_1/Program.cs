d2f05bd4c28f3f279c6256080efd5132aabf1de0
itslisek <michal.migdal@microsoft.wsei.edu.pl>
2021-03-25 14:41:54 +0100

    Wzorki_Prostokąt

---
+++ Wzorki/Wzorki_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Wzorki_1
+{
+    class Program
+    {
+        const char CHAR = '*';
+        static void Star() => Console.Write(CHAR);
+        static void StarLn() => Console.WriteLine(CHAR);
+        static void Space() => Console.Write(" ");
+        static void SpaceLn() => Console.WriteLine(" ");
+        static void NewLine() => Console.WriteLine();
+
+        public static void Prostokat ( int n, int m )
+        {
+            for( int i = 0; i < n; i++ )
+            {
+                Star();
+            }
+            NewLine();
+
+            for( int j = 1; j < m-1; j++)
+            {
+                Star();
+                for (int i = 1; i < n; i++)
+                    Space();
+
+                StarLn();
+            }
+
+            for( int i = 0; i < n; i++)
+            {
+                Star();
+            }
+            NewLine();
+        }
+        public static void Main(string[] args)
+        {
+            Prostokat(5, 7);
+        }
+    }
+}
