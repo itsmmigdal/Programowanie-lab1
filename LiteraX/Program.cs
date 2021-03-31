decfb8ef3844f07544a3052aec804c2d17c2b5ad
itslisek <michal.migdal@microsoft.wsei.edu.pl>
2021-03-31 13:53:26 +0200

    LiterkaX

---
+++ LiteraX/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LiteraX
+{
+    class Program
+    {
+        const char CHAR = '*';
+        static void Star() => Console.Write(CHAR);
+        static void StarLn() => Console.WriteLine(CHAR);
+        static void Space() => Console.Write(" ");
+        static void SpaceLn() => Console.WriteLine(" ");
+        static void NewLine() => Console.WriteLine();
+
+        static void LiteraX(int n)
+        {
+            if (n < 3) throw new ArgumentException("zbyt mały rozmiar");
+            if (n % 2 == 0) n = n + 1;
+
+            //górna połówka
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    Space();
+                Star();
+                for (int j = 0; j < n; j++)
+                    Space();
+                StarLn();
+            }
+            //środek
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
