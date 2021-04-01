a9f69a23b04a133b1fdd0b4bc9f7df6e2cc0a517
itslisek <michal.migdal@microsoft.wsei.edu.pl>
2021-04-01 20:51:22 +0200

    Zadanie_01.04.2021_Lab1

---
+++ Zadanko_Lab1/Zadanko_Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zadanko_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m, wynik;
+            for ( int n = 2; n <= 100; n++ );
+            Console.ReadLine();
+
+            for ( int m = 2; m <= 100; m++ );
+            Console.ReadLine();
+
+            wynik = n++ * m + m++
+            Console.WriteLine("wynik");
+        }
+    }
+}
