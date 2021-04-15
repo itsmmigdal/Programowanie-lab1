3d2a9bb1842078c9054f6c2d5e9c3d7cf9674561
itslisek <michal.migdal@microsoft.wsei.edu.pl>
2021-04-15 14:56:12 +0200

    QuadraticEquation

---
+++ Alt+/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Alt_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int[] data = Array.ConvertAll<string, int>(input.Split(" ", StringSplitOptions.Rem
+            */
+        }
+    }
+    public static void QuadraticEquation(int a, int b, int c)
+    {
+        double ad, bd, cd, x1, x2;
+    }
+}
