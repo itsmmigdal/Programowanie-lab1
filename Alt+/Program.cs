060dda3d3c9e3512c3b903e8bafae343f904ee76
itslisek <michal.migdal@microsoft.wsei.edu.pl>
2021-04-15 14:56:49 +0200

    Update Program.cs

--- Alt+/Program.cs
+++ Alt+/Program.cs
@@ -9,12 +9,21 @@ namespace Alt_
             /*
             int n = int.Parse(Console.ReadLine());
             string input = Console.ReadLine();
-            int[] data = Array.ConvertAll<string, int>(input.Split(" ", StringSplitOptions.Rem
+            int[] data = Array.ConvertAll<string, int>(input.Split(" ", StringSplitOptions
             */
         }
     }
     public static void QuadraticEquation(int a, int b, int c)
     {
-        double ad, bd, cd, x1, x2;
+        double ad, bd, cd, x1, x2; delta;
+        ad = double.Parse(a.ToString());
+        bd = double.Parse(b.ToString());
+        cd = double.Parse(c.ToString());
+
+        if (a==0 && b==0 && c==0)
+        {
+            Console.WriteLine("infinity");
+
+        }
     }
 }
